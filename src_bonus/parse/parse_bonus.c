cfd5b39295075045be0b1f8ab99b05c7cd9594b4
nyhm <523hiloki@gmail.com>
2025-06-07 19:31:58 +0900

    merge

--- src_bonus/parse/parse_bonus.c
+++ src_bonus/parse/parse_bonus.c
@@ -3,20 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   parse_bonus.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: samatsum <samatsum@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hnagashi <hnagashi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 21:46:52 by hnagashi          #+#    #+#             */
-/*   Updated: 2025/06/07 18:41:42 by samatsum         ###   ########.fr       */
+/*   Updated: 2025/06/07 19:30:40 by hnagashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../miniRT_bonus.h"
 
-//t_color		parse_color(char *str);
+// t_color		parse_color(char *str);
 static int	is_empty_or_comment(char *line);
 static void	dispatch_parse(t_scene *scene, char **tokens);
 static void	parse_line(char *line, t_scene *scene);
-//void		parse_rt_file(const char *filename, t_scene *scene);
+// void		parse_rt_file(const char *filename, t_scene *scene);
 
 t_color	parse_color(char *str)
 {
@@ -27,6 +27,11 @@ t_color	parse_color(char *str)
 	rgb_tokens = ft_split(str, ',');
 	if (!rgb_tokens)
 		return (color);
+	if (!rgb_tokens[0] || !rgb_tokens[1] || !rgb_tokens[2])
+	{
+		free_split(rgb_tokens);
+		ft_error("Error: color values (r, g, b) must be between 0 and 255\n");
+	}
 	color.r = ft_atoi(rgb_tokens[0]);
 	color.g = ft_atoi(rgb_tokens[1]);
 	color.b = ft_atoi(rgb_tokens[2]);
@@ -60,7 +65,7 @@ static void	dispatch_parse(t_scene *scene, char **tokens)
 {
 	while (*tokens)
 	{
-		if (ft_strcmp(tokens[0], "A") == 0)//ft_strlen()も組み合わせないと危険！
+		if (ft_strcmp(tokens[0], "A") == 0) // ft_strlen()も組み合わせないと危険！
 			parse_ambient(scene, &tokens);
 		else if (ft_strcmp(tokens[0], "C") == 0)
 			parse_camera(scene, &tokens);
@@ -97,8 +102,8 @@ void	parse_rt_file(const char *filename, t_scene *scene)
 {
 	int		fd;
 	char	*line;
-	int		has_read;// bool?
 
+	int has_read; // bool?
 	has_read = 0;
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
