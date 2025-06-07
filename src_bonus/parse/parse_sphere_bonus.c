67e09baaebf46234bf0852a164c7696d6b09ce3f
Sanshiro Matsumoto <samatsum@c4r1s9.42tokyo.jp>
2025-06-07 20:08:44 +0900

    parse_dir

--- src_bonus/parse/parse_sphere_bonus.c
+++ src_bonus/parse/parse_sphere_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse_sphere_bonus.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hnagashi <hnagashi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: samatsum <samatsum@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/05 16:57:59 by hnagashi          #+#    #+#             */
-/*   Updated: 2025/06/07 16:01:56 by hnagashi         ###   ########.fr       */
+/*   Updated: 2025/06/07 19:58:48 by samatsum         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void		parse_sphere(t_scene *scene, char ***tokens);
 
 void	set_s_checker(t_sphere *s, char ***tokens)
 {
-	if ((*tokens)[4] && (ft_strcmp((*tokens)[4], "checkerboard") == 0
+	if ((*tokens)[4] && (ft_strcmp((*tokens)[4], "checkerboard") == 0 \
 			|| ft_strcmp((*tokens)[4], "checkerboard\n") == 0))
 	{
 		s->checkerboard = 1;
@@ -36,11 +36,11 @@ static void	sphere_check(t_sphere s)
 {
 	if (s.radius <= 0)
 		ft_error("Error: sphere radius must be greater than 0\n");
-	if (s.center.x < -1000 || s.center.x > 1000 || s.center.y < -1000
-		|| s.center.y > 1000 || s.center.z < -1000 || s.center.z > 1000)
+	if (s.center.x < -1000 || s.center.x > 1000 || s.center.y < -1000 \
+			|| s.center.y > 1000 || s.center.z < -1000 || s.center.z > 1000)
 		ft_error("Error: sphere center must be between -1000 and 1000\n");
-	if (s.color.r < 0 || s.color.r > 255 || s.color.g < 0 || s.color.g > 255
-		|| s.color.b < 0 || s.color.b > 255)
+	if (s.color.r < 0 || s.color.r > 255 || s.color.g < 0 || s.color.g > 255 \
+			|| s.color.b < 0 || s.color.b > 255)
 		ft_error("Error: sphere color values must be between 0 and 255\n");
 }
 
@@ -60,7 +60,7 @@ static void	new_sphere(t_scene *scene, t_sphere s)
 	ft_memset(new_arr, 0, sizeof(t_sphere) * new_count);
 	if (scene->spheres && scene->sphere_count > 0)
 	{
-		ft_memcpy(new_arr, scene->spheres, sizeof(t_sphere)
+		ft_memcpy(new_arr, scene->spheres, sizeof(t_sphere) \
 			* scene->sphere_count);
 		free(scene->spheres);
 	}
@@ -75,9 +75,9 @@ void	parse_sphere(t_scene *scene, char ***tokens)
 
 	if (count_array(tokens[0]) < 4)
 		ft_error("Error: invalid sphere line\n");
-	if (!(*tokens)[1] || !(*tokens)[2] || !(*tokens)[3]
-		|| ft_isspace((*tokens)[1][0]) || ft_isspace((*tokens)[2][0])
-		|| ft_isspace((*tokens)[3][0]))
+	if (!(*tokens)[1] || !(*tokens)[2] || !(*tokens)[3] \
+			|| ft_isspace((*tokens)[1][0]) || ft_isspace((*tokens)[2][0]) \
+				|| ft_isspace((*tokens)[3][0]))
 		ft_error("Error: invalid sphere line\n");
 	s.center = parse_vec3((*tokens)[1]);
 	s.radius = ft_atof((*tokens)[2]) / 2.0;
